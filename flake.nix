870b47b6c78021e391414836637d8f81328c52da
Statix <44068961+statix05@users.noreply.github.com>
2025-08-08 17:58:23 +0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "NixOS config for svetos with BSPWM, NVIDIA, Btrfs+LUKS, RAID0 home, HM, AstroNvim";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9P90lC9GO5RZcXcbYwvjw4rHczhBtpR1YwL+8="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    astronvim.url = "github:AstroNvim/AstroNvim";
+  };
+
+  outputs = { self, nixpkgs, home-manager, astronvim }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      nixosConfigurations.svetos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit astronvim; };
+        modules = [
+          ./hosts/svetos/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # Передаём astronvim в home.nix
+            home-manager.extraSpecialArgs = { inherit astronvim; };
+
+            home-manager.users.statix = import ./home/statix/home.nix;
+          }
+        ];
+      };
+    };
+}
