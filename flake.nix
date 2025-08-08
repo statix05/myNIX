b9e8db5c3773a526b8bb0111c68e74d3f9f7aba4
Statix <44068961+statix05@users.noreply.github.com>
2025-08-08 18:42:04 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
-  description = "NixOS config for svetos with BSPWM, NVIDIA, Btrfs+LUKS, RAID0 home, HM, AstroNvim";
+  description = "NixOS config for svetos (BSPWM, NVIDIA, Btrfs+LUKS, RAID0 /home, Home Manager)";
 
   nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9P90lC9GO5RZcXcbYwvjw4rHczhBtpR1YwL+8="
     ];
@@ -12,20 +10,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     astronvim.url = "github:AstroNvim/AstroNvim";
+    astronvim.flake = false; # берём как исходники
   };
 
   outputs = { self, nixpkgs, home-manager, astronvim }:
     let
       system = "x86_64-linux";
+      overlays = [
+        # Если в канале нет picom-pijulius — используем обычный picom
+        (final: prev: {
+          picom-pijulius = (prev.picom-pijulius or prev.picom);
+        })
+      ];
       pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
+        inherit system overlays;
+        config.allowUnfree = true;
       };
     in {
       nixosConfigurations.svetos = nixpkgs.lib.nixosSystem {
@@ -38,10 +42,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
-            # Передаём astronvim в home.nix
             home-manager.extraSpecialArgs = { inherit astronvim; };
-
             home-manager.users.statix = import ./home/statix/home.nix;
           }
         ];
